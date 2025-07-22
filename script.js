4a4f2317c452c10ee673f2aaacd391000675348d
evil-vibe-coder <swanhumi@gmail.com>
2025-07-22 13:19:21 -0700

    reached by most yet understood by some, damnation is not the opposite of salvation, they are in fact, similar

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.querySelector('.nav-toggle')?.addEventListener('click', () => {
 // FAQ Data
 const faqData = [
     ["Which Minecraft versions are supported?", "dlad supports Fabric 1.21 and above. The versions required to play mlum"],
-    ["How do I install?", "Drop the `.jar` into your `mods/` folder and run with the Fabric loader."],
+    ["How do I install?", "<div style='text-align: center;'>Installation is simple:<br><br>1) Make sure you have Java installed (<a href='https://docs.fabricmc.net/players/installing-java/windows' target='_blank'>see Fabric guide</a>)<br><br>2) Download and run the Fabric installer from the <a href='https://fabricmc.net/use/' target='_blank'>official site</a> for Minecraft version 1.21 or later <br><br>3) Download both the <a href='https://www.curseforge.com/minecraft/mc-mods/fabric-api' target='_blank'>Fabric API</a> and <a href='index.html'>dlad mod</a> .jar files for your selected Minecraft version <br><br>4) Place both .jar files in your Minecraft's <a href='https://docs.fabricmc.net/players/installing-mods' target='_blank'><code>mods/</code> folder</a><br><br>5) Launch Minecraft using the <a href='https://docs.fabricmc.net/players/installing-fabric' target='_blank'>Fabric profile</a> in the launcher<br><br>For detailed instructions, visit the <a href='https://docs.fabricmc.net/players/faq' target='_blank'>Fabric Player FAQ</a>.</div>"],
     ["Is this mod detectable?", "No, all features can be disabled and offers a self destruct feature"],
     ["Will there be more features?", "Yes, we're constantly working on new updates! If you got any suggestions, feel free to fill out the form on the feedback page."]
 ];
