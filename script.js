85fd3eb431a794f13d1aed62d5ce1a2ca3c1a954
dladmodmlum <eraulme61+dlad@gmail.com>
2025-08-10 23:40:46 +0200

    fixes,  faq update

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ document.querySelector('.nav-toggle')?.addEventListener('click', () => {
 
 const faqData = [
     ["Which Minecraft versions are supported?", "Fabric 1.21 and above. Any java version"],
+    ["Virus?", "No, the mod is safe to use, scanning it WILL return clean. Always download mods from trusted sources to avoid malware. The only external request the mod makes is to this very same website to check the version and see if an update is available, absolutely no telemetry, no data collection."],
     ["How do I install?", "<div style='text-align: center;'>Installation is simple:<br><br>1) Make sure you have Java installed (<a href='https://docs.fabricmc.net/players/installing-java/windows' target='_blank'>see Fabric guide</a>)<br><br>2) Download and run the Fabric installer from the <a href='https://fabricmc.net/use/' target='_blank'>official site</a> for Minecraft version 1.21 or later <br><br>3) Download both the <a href='https://www.curseforge.com/minecraft/mc-mods/fabric-api' target='_blank'>Fabric API</a> and <a href='index.html'>dlad mod</a> .jar files for your selected Minecraft version <br><br>4) Place both .jar files in your Minecraft's <a href='https://docs.fabricmc.net/players/installing-mods' target='_blank'><code>mods/</code> folder</a><br><br>5) Launch Minecraft using the <a href='https://docs.fabricmc.net/players/installing-fabric' target='_blank'>Fabric profile</a> in the launcher<br><br>For detailed instructions, visit the <a href='https://docs.fabricmc.net/players/faq' target='_blank'>Fabric Player FAQ</a>.</div>"],
     ["Is the mod detectable?", "No, as long as you're responsible and use the features wisely, in itself dlad is not detectable as it's all client side. But be cautious as mods can still see suspicious behaviour."],
     ["Will there be more features?", "We're constantly working on new updates. If you got any suggestions, feel free to fill out the form on the feedback page."],
-];
+    ["Why not open source?", "Open source would mean devs and staff could see how the code works, allowing them to potentially make patches and bypasses. Overall, keeping it closed source helps make the mod's features last longer"],
+    ];
 
 function initFAQ() {
     const faqContainer = document.querySelector('main');
