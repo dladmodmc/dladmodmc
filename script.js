6550a4da1841e2970e776e79bbb74d0fedccf9ec
dladmodmlum <eraulme61+dlad@gmail.com>
2025-08-12 14:12:05 +0200

    mod release paired with source

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ document.querySelector('.nav-toggle')?.addEventListener('click', () => {
 const faqData = [
     ["Which Minecraft versions are supported?", "Fabric 1.21 and above. Any java version"],
     ["How do I open the menu?", "The default key is shift + . This can be changed inside the menu"],
-    ["Virus?", "No, the mod is safe to use, scanning it WILL return clean. Always download mods from trusted sources to avoid malware. The only external request the mod makes is to this very same website to check the version and see if an update is available, absolutely no telemetry, no data collection."],
+    ["Virus?", "Source: <a href='https://github.com/dladmodmc/dladmodsource' target='_blank'>GitHub</a><br>No, the mod is safe to use, scanning it WILL return clean. Always download/update this mod either from THIS website, modrinth or curseforge, we will NEVER send you the mod through discord. The only external request the mod makes is to this very same website to check the version and see if an update is available, absolutely no telemetry, no data collection."],
     ["How do I install?", "<div style='text-align: center;'>Installation is simple, you can either download it from curseforge/modrinth or doing it manually like so:<br><br>1) Make sure you have Java installed (<a href='https://docs.fabricmc.net/players/installing-java/windows' target='_blank'>see Fabric guide</a>)<br><br>2) Download and run the Fabric installer from the <a href='https://fabricmc.net/use/' target='_blank'>official site</a> for Minecraft version 1.21 or later <br><br>3) Download both the <a href='https://www.curseforge.com/minecraft/mc-mods/fabric-api' target='_blank'>Fabric API</a> and <a href='downloads.html'>dlad mod</a> .jar files for your selected Minecraft version <br><br>4) Place both .jar files in your Minecraft's <a href='https://docs.fabricmc.net/players/installing-mods' target='_blank'><code>mods/</code> folder</a><br><br>5) Launch Minecraft using the <a href='https://docs.fabricmc.net/players/installing-fabric' target='_blank'>Fabric profile</a> in the launcher<br><br>For detailed instructions, visit the <a href='https://docs.fabricmc.net/players/faq' target='_blank'>Fabric Player FAQ</a>.</div>"],
     ["Is the mod detectable?", "No, as long as you're responsible and use the features wisely, in itself dlad is not detectable as it's all client side. But be cautious as mods can still see suspicious behaviour."],
     ["Will there be more features?", "We're constantly working on new updates. If you got any suggestions, feel free to fill out the form on the feedback page."],
-    ["Why not open source?", "Open source would mean devs and staff could see how the code works, allowing them to potentially make patches and bypasses. Overall, keeping it closed source helps make the mod's features last longer. This also explains why it only works in mlum"],
+    ["Open source?", "Source: <a href='https://github.com/dladmodmc/dladmodsource' target='_blank'>GitHub</a><br>Yes, but partially. Every part of the code is able to be seen except for how the features detect and how the mod is enabled/disabled depending on if you're in mlum or not, these measures are set to prevent staff from making patches."],
     ];
 
 function initFAQ() {
@@ -63,7 +63,8 @@ const changelogData = [
     {
         version: "1.0",
         changes: [
-            "Still WIP",
+            "Initial release",
+            "To be added (v1.1): Escape detector sound change, warning size edit size for each feature and test button"
         ]
     }
 ];
